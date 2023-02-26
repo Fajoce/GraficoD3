b6242e1fe6ff0a2bb8d2aeed58a860d1f91634a8
Nicolle <fajoce@gmail.com>
2023-02-26 13:22:35 -0500

    delete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let data = [];
+var gr;
 function Grafico() {
-  let widthBar = 25;
-  d3.select("div.chart")
+  gr = d3.select("div.chart")
     .selectAll("svg")
     .data(data)
     .enter()
@@ -32,26 +32,42 @@ function addData() {
 }
 function limpiar() {
   document.getElementById("valores").value = "";
+  document.getElementById("nuevo").value = "";
 }
 //https://codepen.io/fajoce/pen/qBMaZPq
 function updateData() {
   debugger;
-  
+
   let actual = document.getElementById("valores")
   let nuevo = document.getElementById("nuevo")
-  if(actual != '' || nuevo != ''){
-  while (data.indexOf(actual.value) != -1) {
-    data.splice(data.indexOf(actual.value), 1, nuevo.value);
+  if (actual.value != '' || nuevo.value != '') {
+    while (data.indexOf(actual.value) != -1) {
+      data.splice(data.indexOf(actual.value), 1, nuevo.value);
+    }
+    gr.remove();
+    limpiar();
+    Grafico();
+  }
+  else {
+    alert('Nada que actualizar')
+    exit
   }
 }
-else{
-  alert('Nada que actualizar')
-  //exit
-}
-  limpiarAreaGrafico();
-  Grafico();
+function deleteData() {
+  debugger;
+  let a = document.getElementById("valores");
+  if (a.value != "") {
+    while (data.indexOf(a.value) != -1) {
+      data.splice(data.indexOf(a.value), 1);
+    }
+    gr.remove();
+    limpiar();
+    Grafico();
+    alert("graph edited successfully!");
+    }
+  else {
+    alert("Nothing to remove!");
+  }
 }
 
-function limpiarAreaGrafico(){
-document.getElementsByClassName('chart').innerHtml = ""
-}
+
